eae385a86d9cb7c327fa96b28d5ba0b7f5a926f5
George Walkey <gwalkey@users.noreply.github.com>
2019-09-12 21:04:12 -0400

    Add files via upload

---
+++ 03_SSRS_REST_Update_Report_Datasource_Credentials.ps1
@@ -0,0 +1,66 @@
+﻿# ---
+# Update SSRS Report Data Sources
+# ---
+
+$SSRSServer='localhost'
+$URI = "http://$SSRSServer/reports/api/v2.0"
+
+# Turn On TLS over SSL
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
+# -------------------------------------------------
+# Change Datasource Creds on Report "Rest Demo 1"
+# -------------------------------------------------
+$rpt= Invoke-RestMethod "$URI/Reports(path='/Rest Demo 1')" -Method get -UseDefaultCredentials
+$rptID = $rpt.Id
+
+$rptProps= Invoke-RestMethod "$URI/Reports($rptID)/DataSources" -Method get -UseDefaultCredentials
+$rptProps.value[0]
+
+# Set Breakpoint Here
+$rptProps.value[0].CredentialRetrieval='store'
+
+# Add Property to Object
+if ($rptProps.value[0].CredentialsInServer -eq $null)
+{
+    $myObject = [PSCustomObject]@{
+        UserName = 'rdemo'
+        Password = 'rdemo'
+        UseAsWindowsCredentials = 'false'
+        ImpersonateAuthenticatedUser = 'false'
+    }
+
+    $rptProps.value[0].PSObject.Properties.Remove('CredentialsInServer')
+    Add-Member -InputObject $rptProps.value[0] -MemberType NoteProperty -Name CredentialsInServer -Value $myObject
+}
+else
+{
+    $rptProps.value[0].CredentialsInServer.UserName='rdemo'
+    $rptProps.value[0].CredentialsInServer.Password='rdemo'
+    $rptProps.value[0].CredentialsInServer.UseAsWindowsCredentials=$false
+    $rptProps.value[0].CredentialsInServer.ImpersonateAuthenticatedUser=$false
+}
+  
+$newJSON = $rptProps.value | convertto-json -Depth 4
+$newJSON = '['+$newJSON  +']'
+
+$params = @{
+    ContentType = 'application/json'
+    Headers = @{'accept'='application/json'}
+    Body = $newJSON
+    Method = 'Put'
+    URI = $URI+'/Reports('+$RPTID+')/DataSources'
+}
+
+try
+{
+    Invoke-RestMethod @params  -UseDefaultCredentials -ErrorAction Stop
+}
+catch {
+
+    $_.Exception.Response
+    
+    $_.Exception.Message
+    
+    $_.ErrorDetails.Message    
+ }
